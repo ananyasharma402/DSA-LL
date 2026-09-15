c5153843a91d9f2ac6567fcfb59613ec149ff578
ananyasharma402 <ananyaasharma341@gmail.com>
2026-09-15 18:07:55 +0530

    Add string lowercase conversion

---
+++ Lowercasestring.java
@@ -0,0 +1,12 @@
+public class Lowercasestring {
+    public static void main(String[] args) {
+
+        String str = "HeLLo WoRLD";
+
+        String result = str.toLowerCase();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Lowercase String: " + result);
+    }
+
+}
